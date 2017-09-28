9256e81e39077a2f34a12cd97accc7cedd30113d
David Aguilar <davidaguilarh.94@gmail.com>
2017-09-27 20:23:23 -0500

    try 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  namespace :api, defaults: {format: 'json'} do
+  namespace :api do
     namespace :v1 do
-      resources :products
+      resources :products, , defaults: {format: 'json'} 
     end
   end
   resources :products
