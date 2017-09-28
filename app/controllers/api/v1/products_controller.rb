f7f635c4d22f067d0a8a82b075c2c68bfc67ac16
David Aguilar <davidaguilarh.94@gmail.com>
2017-09-27 20:43:27 -0500

    try 9

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,9 +2,10 @@ module Api
   module V1
     class ProductsController < ApplicationController
       protect_from_forgery with: :null_session
-      {format: 'json'}
+
 
       def index
+        {format: 'json'}
         @products = Product.all
         render json: @products
       end
