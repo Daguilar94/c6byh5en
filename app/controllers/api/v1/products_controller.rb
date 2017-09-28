5889938d0e7106b50a8f14004c69f25baf62f1b7
David Aguilar <davidaguilarh.94@gmail.com>
2017-09-27 22:50:22 -0500

    dont know anymore...

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  module V1
+  module V1 
     class ProductsController < ApplicationController
       protect_from_forgery with: :null_session
 
@@ -7,7 +7,7 @@ module Api
         @products = Product.all
         render json: @products
       end
-      
+
     end
   end
 end
